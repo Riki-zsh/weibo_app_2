9d8ec7736793f6c446e8cdb659d544ba929ab1db
zy <a1424588331>
2021-06-09 22:46:55 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use Auth;
 
 class UserController extends Controller
 {
@@ -37,6 +38,7 @@ class UserController extends Controller
             'email' => $request->email ?? '',
             'password' => bcrypt($request->password ?? '')
         ]);
+        Auth::login($user);
         session()->flash('success', "欢迎，您将在这里开启一段新的旅程~");
         return redirect()->route('user.show', [$user]);
     }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,9 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-{{--  mix()添加哈希值解决浏览器缓存问题--}}
-  <link rel="stylesheet" href=" {{mix('/css/app.css')}}">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
 @include('layouts._header')
 
@@ -15,5 +15,7 @@
     @include('layouts._footer')
   </div>
 </div>
+
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
